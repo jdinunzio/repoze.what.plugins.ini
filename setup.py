d0ce3202f8abb0b0d84515796c413b05d48baac3
José Dinuncio <jdinunci@uc.edu.ve>
2016-03-17 12:44:03 -0430

    Updates setup.py to use README.rst

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
+README = open(os.path.join(here, 'README.rst')).read()
 version = open(os.path.join(here, 'VERSION.txt')).readline().rstrip()
 
 setup(name='repoze.what.plugins.ini',
